505b55502d23fe985f41200ac7e995544c92e230
Hiatt <ah412415@ohio.edu>
2020-01-24 11:18:31 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ah412415_MIS4200.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "My MIS4200 description.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "This is my contact Info.";
+
+            return View();
+        }
+    }
+}
