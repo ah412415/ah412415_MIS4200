0487584150d9515bef0406d6b015744bc99eece9
Alexandra Hiatt <ah412415@ohio.edu>
2020-02-10 16:46:26 -0500

    sd2 again

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -8,7 +8,7 @@ namespace ah412415_MIS4200.Models
 {
     public class Appointment
     {
-        public int appointmentDetailID { get; set; }
+        public int appointment { get; set; }
         public int DateTime { get; set; }
         public decimal roomNumber { get; set; }
         public string condition { get; set; }
